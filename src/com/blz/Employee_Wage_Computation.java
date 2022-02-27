df9deccef37f1ccc341b15067cd9433f4d6af4b7
Priyanka <priyankadombale27@gmail.com>
2022-02-27 17:02:07 +0530

    Added : Calculate Employee Daily Wage

--- src/com/blz/Employee_Wage_Computation.java
+++ src/com/blz/Employee_Wage_Computation.java
@@ -4,18 +4,24 @@ public class Employee_Wage_Computation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Employee Wage Compution..!!!");
-		checkPresentOrNot();
+		calculateDailyWage(); // Calling method
 	}
 
-	// Use static method to check Employee present or not.
+	// Use static method to Calculate Employee Daily Wage.
 
-	static void checkPresentOrNot() {
+	static void calculateDailyWage() {
 		int randomNum = (int) (Math.random() * 2); // create random number using math function
+		int wagePerHour = 20;
+		int empHrs = 0;
+		int empHrsf = 8;
 
 		if (randomNum == 1) // Check Employee presence
 		{
-			System.out.println(" Employee is Present " + randomNum);
-		} else
-			System.out.println(" Employee is Absent " + randomNum);
+			int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
+			System.out.println(" Employee is Present, salary of Full time is :" + salary);
+		} else {
+			int salary = wagePerHour * empHrs; // No Working , No Salary
+			System.out.println(" Employeeis Absent, salary is :" + salary);
+		}
 	}
 }
