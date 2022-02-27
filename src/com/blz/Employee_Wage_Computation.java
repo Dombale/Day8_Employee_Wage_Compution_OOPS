9f6adcfd172e883cfda6c922c65fd2b18626c74b
Priyanka <priyankadombale27@gmail.com>
2022-02-27 16:47:45 +0530

    Added: Check Employee Present OR not by calling Method

--- src/com/blz/Employee_Wage_Computation.java
+++ src/com/blz/Employee_Wage_Computation.java
@@ -4,6 +4,18 @@ public class Employee_Wage_Computation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Employee Wage Compution..!!!");
+		checkPresentOrNot();
 	}
 
+	// Use static method to check Employee present or not.
+
+	static void checkPresentOrNot() {
+		int randomNum = (int) (Math.random() * 2); // create random number using math function
+
+		if (randomNum == 1) // Check Employee presence
+		{
+			System.out.println(" Employee is Present " + randomNum);
+		} else
+			System.out.println(" Employee is Absent " + randomNum);
+	}
 }
