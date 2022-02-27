f9d68228f38c96ccea9def3a315b32caa0821a1d
Priyanka <priyankadombale27@gmail.com>
2022-02-27 17:12:34 +0530

    Added : Add Part time Employee and Wage

--- src/com/blz/Employee_Wage_Computation.java
+++ src/com/blz/Employee_Wage_Computation.java
@@ -1,27 +1,37 @@
 package com.blz;
 
 public class Employee_Wage_Computation {
+	static int wagePerHour = 20;
+	static int empHrs = 0;
+	static int empHrsf = 8;
+	static int empHrsP = 4;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in Employee Wage Compution..!!!");
-		calculateDailyWage(); // Calling method
+		addPartTimeEmpage(); // Calling method
 	}
 
 	// Use static method to Calculate Employee Daily Wage.
 
-	static void calculateDailyWage() {
+	static void addPartTimeEmpage() {
 		int randomNum = (int) (Math.random() * 2); // create random number using math function
-		int wagePerHour = 20;
-		int empHrs = 0;
-		int empHrsf = 8;
 
 		if (randomNum == 1) // Check Employee presence
 		{
-			int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
-			System.out.println(" Employee is Present, salary of Full time is :" + salary);
+			System.out.println(" Employee is Present ");
+
+			if (empHrsf == 8) {
+				int salary = wagePerHour * empHrsf; // Employee Full timeSalary/Day
+				System.out.println(" Employee salary of Full time is :" + salary);
+			}
+			if (empHrsP == 4) {
+				int salary = wagePerHour * empHrsP; // Employee Part time Salary/Day
+				System.out.println(" Employee salary of Part time is :" + salary);
+			}
 		} else {
-			int salary = wagePerHour * empHrs; // No Working , No Salary
-			System.out.println(" Employeeis Absent, salary is :" + salary);
+			System.out.println(" Employee is Absent ");
+			int salary = wagePerHour * empHrs; // Employee No Salary/Day
+			System.out.println(" Employee salary is :" + salary);
 		}
 	}
 }
